be760ef3be649a42fc4097088b50cb6079914366
shivram yadav <shivramyadav52@gmail.com>
2021-07-08 20:49:38 +0530

    some change made in order.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ server.listen(port, () => {
     console.log('server is running on port ' + port)
 });
 
+//  https://gentle-everglades-52420.herokuapp.com/ deployed to Heroku
 // /c/Program\ Files/Heroku/bin/heroku login
 // heroku create
 // git push heroku master
